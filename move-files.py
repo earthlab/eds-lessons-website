d9ab1428c685921b2142b9d458996d406f004036
Leah Wasser <leah.wasser@colorado.edu>
2019-08-12 18:13:58 -0600

    cleanup

--- move-files.py
+++ move-files.py
@@ -7,12 +7,14 @@ base_repo = "dummy-website-test"
 base_path = "~/"
 #base_path = "../"
 
-ignore_extensions =  [".yml"]
+print("Now doing stuff")
+print("Now doing stuff")
 
 # Open the text file and move files over to the other dir
 fp = open('changed-files.txt', 'r')
 
 for f in fp:
+    print(f)
     if not f.lower().endswith(('.yml')):
         print(f)
         new_path = os.path.join(base_path, base_repo, f)
