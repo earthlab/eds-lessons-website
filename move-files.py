6956fb5c48376f019179abadb3adbcc0e2c498d4
Leah Wasser <leah.wasser@colorado.edu>
2019-08-12 18:30:32 -0600

    stupid mistakes

--- move-files.py
+++ move-files.py
@@ -16,20 +16,24 @@ os.getcwd()
 
 for f in fp:
     print("Trying to move: ", f)
+    f = f.rstrip('\n')
     if not f.lower().endswith(('yml', 'py')):
-        f = f.rstrip('\n')
         print("final file name: ", f)
         new_path = os.path.join(base_path, base_repo, f)
         dir_path = os.path.dirname(new_path)
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
-        print("moving to: ", new_path)
         shutil.copy(f, new_path)
+        print("File has been moved to: ", new_path)
+    else:
+        print("sorry can't move: ", f)
 
 
 
 
-
+f = "move-files.py"
+if not f.lower().endswith(('yml', 'py')):
+    print("Test")
 
 
 
