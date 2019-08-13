944c650abafb73a4bd8dbd25bef8dd647bf34417
Leah Wasser <leah.wasser@colorado.edu>
2019-08-12 18:03:24 -0600

    move files scripts

---
+++ move-files.py
@@ -0,0 +1,31 @@
+
+
+
+import os
+import shutil
+base_repo = "dummy-website-test"
+base_path = "~/"
+#base_path = "../"
+
+ignore_extensions =  [".yml"]
+
+# Open the text file and move files over to the other dir
+fp = open('changed-files.txt', 'r')
+
+for f in fp:
+    if not f.lower().endswith(('.yml')):
+        print(f)
+        new_path = os.path.join(base_path, base_repo, f)
+        dir_path = os.path.dirname(new_path)
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        print(new_path)
+        shutil.copy(f, new_path)
+
+
+
+
+
+
+
+
