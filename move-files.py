acdff7f99ceaff8c57659f4d02d0bebeb6ec6961
Leah Wasser <leah.wasser@colorado.edu>
2019-08-12 18:26:27 -0600

    test

--- move-files.py
+++ move-files.py
@@ -7,24 +7,23 @@ base_repo = "dummy-website-test"
 base_path = "~/"
 #base_path = "../"
 
-print("Now doing stuff")
-print("Now doing stuff")
 
 # Open the text file and move files over to the other dir
 fp = open('changed_files.txt', 'r')
 print("here is the file: ", fp)
 
+os.getcwd()
+
 for f in fp:
-    print("starting the loop now")
-    print(f)
-    if not f.lower().endswith(('yml')):
+    print("Trying to move: ", f)
+    if not f.lower().endswith(('yml', 'py')):
         f = f.rstrip('\n')
-        print(f)
+        print("final file name: ", f)
         new_path = os.path.join(base_path, base_repo, f)
         dir_path = os.path.dirname(new_path)
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
-        print(new_path)
+        print("moving to: ", new_path)
         shutil.copy(f, new_path)
 
 
