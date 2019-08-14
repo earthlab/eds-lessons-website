0209cadf230dec54ec7acf5512aee92bee58298d
Leah Wasser <leah.wasser@colorado.edu>
2019-08-14 15:24:50 -0600

    testing to see if there are files to commit

--- parse-commit.py
+++ parse-commit.py
@@ -1,7 +1,7 @@
-'''
+"""
 This script reads every line of "changed_files.txt" and extracts only the files
 that should be moved to the live website.
-'''
+"""
 
 with open("changed_files.txt") as f:
     content = f.readlines()
@@ -10,7 +10,9 @@ content = [x.rstrip('\n').strip() for x in content]
 
 website_files = [line for line in content if not line.endswith((".py", "yml"))]
 
-with open("website_files.txt", "w") as f:
-    if len(website_files) > 0:
-        for fn in website_files:
-            f.write("%s\n" % fn)
+if len(website_files) > 0:
+    with open("website_files.txt", "w") as f:
+        if len(website_files) > 0:
+            for count, fn in enumerate(website_files):
+                f.write("%s\n" % fn)
+
