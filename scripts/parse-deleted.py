c5285d8c91fb372bf6c2bca0ad6c9d2cb6a74572
Leah Wasser <leah.wasser@colorado.edu>
2020-02-06 18:12:52 -0700

    update ci to remove images and posts when needed

---
+++ scripts/parse-deleted.py
@@ -0,0 +1,25 @@
+'''
+This script reads every line of "deleted_files.txt" and stores each line as an element in a list
+From this list, we then parse out .ipynb files, .Rmd files, and image files (.jpg, .jpeg, .gif)
+'''
+
+with open("deleted_files.txt") as f:
+    content = f.readlines()
+content = [x.strip() for x in content]
+
+deleted_md_files = [line for line in content if line.endswith(".md")]
+deleted_image_files = [line for line in content if line.endswith(".jpg") or line.endswith(".jpeg") or line.endswith(".gif") or line.endswith(".png")]
+
+print("deleted notebooks: ", deleted_md_files)
+print("deleted image files: ", deleted_image_files)
+
+
+with open("deleted_md_files.txt", "w") as f:
+    if len(deleted_md_files) > 0:
+        for fn in deleted_md_files:
+            f.write("%s\n" % fn)
+
+with open("deleted_image_files.txt", "w") as f:
+    if len(deleted_image_files) > 0:
+        for fn in deleted_image_files:
+            f.write("%s\n" % fn)
