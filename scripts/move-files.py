1709990cc970b5b34de069dc7787c9e36bfc66f0
Leah Wasser <leah.wasser@colorado.edu>
2020-02-13 17:23:30 -0700

    DONT MERGE -- Test week 1 (#63)
    * update ci to remove images and posts when needed
    
    * Ea python delete week 1 (#62)
    
    * Add new section on spatial data applications to Intermediate TB (#506) 1-additional-content README.md blog changed_files.txt changed_image_files.txt changed_notebooks.txt changed_rmd_files.txt courses current_branch.txt images keys scripts tutorials workshops Copy images for remote sensing dir to images/earth-analytics
     
     1-additional-content README.md blog changed_files.txt changed_image_files.txt changed_notebooks.txt changed_rmd_files.txt courses current_branch.txt images keys scripts tutorials workshops Add lessons for new section on spatial data apps
     
     1-additional-content README.md blog changed_files.txt changed_image_files.txt changed_notebooks.txt changed_rmd_files.txt courses current_branch.txt images keys scripts tutorials workshops Small edits to clean up lessons
     
     1-additional-content README.md blog changed_files.txt changed_image_files.txt changed_notebooks.txt changed_rmd_files.txt courses current_branch.txt images keys scripts tutorials workshops Small text updates to pgs 1 and 4
     
     1-additional-content README.md blog changed_files.txt changed_image_files.txt changed_notebooks.txt changed_rmd_files.txt courses current_branch.txt images keys scripts tutorials workshops Update chapter name
    
    * Delete moved lessons from week 1 ea python course
    
    Co-authored-by: earth-lab <42077395+earth-lab@users.noreply.github.com>
    
    Co-authored-by: earth-lab <42077395+earth-lab@users.noreply.github.com>

---
+++ scripts/move-files.py
@@ -0,0 +1,40 @@
+"""
+This script takes a set of files from a text file, and line by line parses and
+moves them to the corresponding directory.
+
+It is used to move files in the website repo to the eds.org live website. 
+This script assumes that it is being run in the website repo.
+"""
+
+import os
+import shutil
+
+# get absolute path
+cwd = os.path.abspath(os.getcwd())
+# create full path to other repo
+eds_website_repo = cwd.replace("eds-lessons-website", "earthlab.github.io")
+
+changed_files = "website_files.txt"
+
+# Open the text file and move files over to the other dir
+fp = open(changed_files, 'r')
+
+if os.stat(changed_files).st_size == 0:
+    print("There are no changes to move.")
+# Loop through each file, clean the path and move to the final directory
+else:
+    for f in fp:
+        print("Trying to move: ", f)
+        f = f.rstrip('\n').strip()
+        if not f.lower().endswith(('yml', 'py')):
+            print("final file name: ", f)
+            new_path = os.path.join(eds_website_repo, f)
+            dir_path = os.path.dirname(new_path)
+            if not os.path.exists(dir_path):
+                os.makedirs(dir_path)
+            shutil.copy(f, new_path)
+            print("File has been moved to: ", new_path)
+
+
+
+
